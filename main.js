e9b106429048fb81a25d2016084d86012d5833d0
Sarah <sarah.akus@gmail.com>
2020-05-13 23:23:27 -0400

    IT WORKSgit add main.js

--- main.js
+++ main.js
@@ -1,135 +1,169 @@
+let song = new Audio(
+  "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Kevin_MacLeod/Oddities/Kevin_MacLeod_-_Winner_Winner.mp3"
+);
 
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-
-// class Game {
-
-let player = {
-    x: 210 + Math.floor(Math.random() * 160),
-    y: 250,
-    draw: function() { context.fillRect(this.x, this.y, 40, 40); }
-}
-
-let target = {
-    x: 190 + Math.floor(Math.random() * 181),
-    y: 60 + Math.floor(Math.random() * 100),
-    draw: function() { 
-        context.fillRect(this.x, this.y, 20, 20); 
-    } 
-}
-
-class Enemy {
-    constructor() {
-        this.x = 190 + Math.floor(Math.random() * 181),
-        this.y = 60 + Math.floor(Math.random() * 60),
-        this.width = 25,
-        this.height = 25
-        }
-        draw() { 
-            context.fillRect(this.x, this.y, 25, 25); 
-         } 
-        moveY() {
-            this.y+=1.5
-            if(this.y > 270) {
-                this.y = 50
-                this.x = 190 + Math.floor(Math.random() * 181)
-            }
-            
-        }
-        moveX() {
-            this.x+=1.5
-            if(this.x > 400){
-                this.x = 200
-                this.y = 100 + Math.floor(Math.random() * 100)
-            }    }
-    }
-
-let e1 = new Enemy() 
-let e2 = new Enemy()
-setInterval (loop, 33);
-
-// **colliding()** returns true if two passed bodies are colliding.
-// The approach is to test for five situations.  If any are true,
-// the bodies are definitely not colliding.  If none of them
-// are true, the bodies are colliding.
-// 1. b1 is the same body as b2.
-// 2. Right of `b1` is to the left of the left of `b2`.
-// 3. Bottom of `b1` is above the top of `b2`.
-// 4. Left of `b1` is to the right of the right of `b2`.
-// 5. Top of `b1` is below the bottom of `b2`.
-function colliding (b1, b2) {
-    return !(
-      b1 === b2 ||
-          b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
-          b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
-          b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
-          b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
-    )
+class Game {
+  constructor() {
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
+    let gameSize = { x: canvas.width, y: canvas.height };
+    this.score = 0;
+    this.enemy1 = new Enemy();
+    this.enemy2 = new Enemy();
+    this.enemy3 = new Enemy();
+    this.enemy4 = new Enemy();
+    this.target = new Target();
+    this.player = new Player(gameSize);
+    let animate = () => {
+      if (colliding(this.player, this.target)) {
+        console.log("target hit");
+        this.score += 10;
+        let pointsTally = document.querySelector("#points-tally");
+        pointsTally.textContent = this.score;
+        this.target = new Target();
+      }
+      if (
+        colliding(this.player, this.enemy1) ||
+        colliding(this.player, this.enemy2) ||
+        colliding(this.player, this.enemy3) ||
+        colliding(this.player, this.enemy4)
+      ) {
+        console.log("collision");
+        window.alert("Game over, you lose!");
+        window.location.reload();
+        return;
+      }
+      context.fillStyle = "yellow";
+      context.fillRect(0, 0, gameSize.x, gameSize.y);
+      context.fillStyle = "black";
+      this.enemy1.moveY();
+      this.enemy1.draw(context);
+      this.enemy2.moveX();
+      this.enemy2.draw(context);
+      this.enemy3.moveZ();
+      this.enemy3.draw(context);
+      this.enemy4.moveW();
+      this.enemy4.draw(context);
+      context.fillStyle = "red";
+      this.target.draw(context);
+      this.drawPlayer(context, gameSize);
+      this.player.movePlayer();
+      requestAnimationFrame(animate);
+    };
+    animate();
   }
 
+  drawScore() {
+    context.font = "16px Arial";
+    context.fillText("Score: " + score);
+  }
 
-function collisionEnemyOne(e1, player) {
-    if ( 
-        // e1.x + e1.width / 2 < player.x - player.width / 2 ||
-        e1.y + e1.height > player.y + player.height) {
-        // e1.x - e1.width / 2 > player.x + player.width / 2 ||
-        // e1.y - e1.height / 2 > player.y + player.height / 2)) {
-            console.log("you lose the game")
-        }
+  drawPlayer(context, gameSize) {
+    context.fillStyle = "blue";
+    let startingXPosition = this.player.x;
+    let startingYPosition = this.player.y;
+    let playerWidth = this.player.size.x;
+    let playerHeight = this.player.size.y;
+    context.fillRect(
+      startingXPosition,
+      startingYPosition,
+      playerWidth,
+      playerHeight
+    );
+  }
+}
+class Player {
+  constructor(gameSize) {
+    this.size = { x: 30, y: 30 };
+    this.x = 320;
+    this.y = 320;
+  }
+
+  movePlayer() {
+    if (Keyboarder.isDown(37)) this.x -= 4;
+    if (Keyboarder.isDown(39)) this.x += 4;
+    if (Keyboarder.isDown(38)) this.y -= 4;
+    if (Keyboarder.isDown(40)) this.y += 4;
+    if (this.x > 320) this.x = 320;
+    if (this.x < 0) this.x = 0;
+    if (this.y < 0) this.y = 0;
+    if (this.y > 320) this.y = 320;
+  }
+}
+class Enemy {
+  constructor() {
+    this.size = { x: 20, y: 20 };
+    this.x = Math.floor(Math.random() * 300);
+    this.y = Math.floor(Math.random() * 300);
+  }
+  draw(context) {
+    context.fillRect(this.x, this.y, 25, 25);
+    context.fillStyle = "black";
+  }
+
+  moveW() {
+    this.y -= 1;
+    if (this.y < 0) {
+      this.y = 500;
+      this.x = Math.floor(Math.random() * 300);
     }
+  }
 
-function collisionEnemyTwo(e2, player) {
-    if((e2.x + e2.width) >= player.x &&
-        e2.x <= (player.x + player.width) &&
-        (e2.y + e2.height) >= player.y &&
-        e2.y <= (player.y + player.height)) 
-            console.log("you lose the game")
+  moveY() {
+    this.y += 1;
+    if (this.y > 350) {
+      this.y = 0;
+      this.x = Math.floor(Math.random() * 300);
     }
-
-function collisionTarget(target, player) {  
-    if((target.x + target.width) >= player.x &&
-        target.x <= (player.x + player.width) &&
-        (target.y + target.height) >= player.y &&
-        target.y <= (player.y + player.height)) 
-            console.log("you win the game")
+  }
+  moveX() {
+    this.x += 1;
+    if (this.x > 350) {
+      this.x = 0;
+      this.y = Math.floor(Math.random() * 300);
+    }
+  }
+  moveZ() {
+    this.x -= 1;
+    if (this.x < 0) {
+      this.x = 350;
+      this.y = Math.floor(Math.random() * 300);
+    }
+  }
 }
 
-function loop() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    if(Keyboarder.isDown(37))
-        player.x-=10;
-    if(Keyboarder.isDown(39))
-        player.x+=10;
-    if(Keyboarder.isDown(38))
-        player.y-=10;
-    if(Keyboarder.isDown(40))
-        player.y+=10;
-    if(player.x > 380)
-        player.x = 380;
-    if(player.x < 190)
-        player.x = 190;
-    if(player.y < 60)
-        player.y = 60
-    if(player.y > 250)
-        player.y = 250
-    context.fillStyle = "yellow"
-    context.fillRect(180, 50, 250, 250);
-    context.fillRect(260, 90, 15, 15)
-    context.fillStyle = "red"
-    target.draw();
-    context.fillStyle = "black"
-    player.draw();
-    context.fillStyle = "blue"
-    e1.moveY();
-    e1.draw();
-    collisionEnemyOne (e1, player);
-    // e2.moveX();
-    // e2.draw();
-    
+class Target {
+  constructor(gameSize) {
+    this.x = Math.floor(Math.random() * 300);
+    this.y = Math.floor(Math.random() * 300);
+    this.size = { x: 20, y: 20 };
+  }
+  draw(context) {
+    context.fillRect(this.x, this.y, 20, 20);
+  }
 }
-    
-    // collisionEnemyTwo (e2, player);
-    collisionTarget (target, player);
 
+function colliding(b1, b2) {
+  return !(
+    b1 === b2 ||
+    b1.x + b1.size.x < b2.x ||
+    b1.y + b1.size.y < b2.y ||
+    b1.x > b2.x + b2.size.x ||
+    b1.y > b2.y + b2.size.y
+  );
+}
 
+function setPlayPause() {
+  playpause = !playpause;
+  if (playpause) {
+    document.getElementById("playpause").value = "Pause";
+    alert("Game has been paused");
+    return;
+  }
+}
 
+new Game();
+window.addEventListener("keydown", (event) => {
+  song.play();
+});
+setPlayPause();
